08ebd4e9320f5e6da4d6f5b8ee27af5168678f69
Alston <alston005@gmail.com>
2020-10-12 15:17:45 -0500

    added instructions to datacleaning file

---
+++ 0. Data cleaning.R
@@ -0,0 +1,11 @@
+# This is the script that we will use for data cleaning. 
+# ----------------
+# Instructions
+# ----------------
+# 1. Please navigate to the 'datacleaning' branch when making changes to this file. 
+# 2. Please commit every small change and give a clear description of the change in the commit message
+# 3. Please comment on the code as clearly as possible
+# --------------------------
+
+# Importing the dataset
+df1 <- xxx
