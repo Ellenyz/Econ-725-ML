1fdbf760c0c95a05d35180ad8e399ee9ed91668c
Ellen <58341868+Ellenyz@users.noreply.github.com>
2020-11-08 17:26:32 -0600

    Keep only Yuzhou's part

--- 0. Data cleaning.R
+++ 0. Data cleaning.R
@@ -7,100 +7,13 @@
 # 3. Please comment on the code as clearly as possible
 # --------------------------
 
-
-
 library("haven")
 library(tidyverse) #includes the dplyr and tidyr packages
 library(dplyr)
 library(tidyr)
 
-# Importing the dataset
-
 df1 <- read_dta("temp.dta")
 orig_data <- data.frame(df1)
-
-dim(orig_data)
-# We have 146734 rows and 605 variables. 
-
-# Remove obviously unnecessary columns (PW)
-# -----------------------------------------
-orig_data = orig_data[ , -which(colnames(orig_data) %in% c("membersince","name","vin","highbiddername",
-                                                           "sellername","questions","X_merge","temp","biddername1","biddername2",
-                                                           "biddername3", "biddername4", "biddername5","biddername6","biddername7",
-                                                           "biddername8","biddername9","biddername10","biddername11","biddername12",
-                                                           "biddername13","biddername14","biddername15","biddername16","biddername17",
-                                                           "biddername18","biddername19","biddername20","biddername21","biddername22"))]
-
-dim(orig_data)
-# We have 146734 rows and 575 variables (We removed 30 variables).
-
-# Fixing date and time date and time (JL)
-# --------------------------------------
-## generally, two types of datetime vars here: standard and numeric
-## turn standard to time var:
-
-orig_data$enddate = strptime(orig_data$enddate, "%b-%d-%y %H:%M:%S")
-orig_data$startdate = strptime(orig_data$startdate, 
-                               "%b-%d-%y %H:%M:%S")
-### auction last in mins
-orig_data$auc_duration_mins = as.numeric(difftime(orig_data$enddate,
-                                                  orig_data$startdate,
-                                                  units="mins"))
-
-# Note: 
-#   1) The var "length" is auction lasts in days does the same thing
-#   2) Note that any var that link "bid" and "1" (eg. logbid1) relates to the highest bid
-
-# Deal with nemeric time var:
-
-### Calculate how long it takes for the highest bidder to make decision (in days)
-
-orig_data$maxbidtime = orig_data$biddate1 - orig_data$startingdate
-
-# Note: I think bidhour, bidminute, bidsec, etc should not be necessary as the best way to
-#     handle time is to calculate duration i.e. time difference it doesn't make sense to 
-#     include a numeric value i.e. bidhour
-
-# 1. Use the "enddate", "startdate" to calculate month indicator (1,...,12)
-
-orig_data$start_m = match(substr(orig_data$startdate, 1, 3), month.abb)
-orig_data$end_m = match(substr(orig_data$enddate, 1, 3), month.abb)
-
-# 2. Use month indicator to calculate season
-
-# ERROR: attach(orig_data)
-orig_data$start_s <- ifelse(start_m==12 | start_m==1 | start_m==2, "Winter", 
-                            ifelse(start_m==3 | start_m==4 | start_m==5, "Spring", 
-                                   ifelse(start_m==6 | start_m==7 | start_m==8, "Summer",
-                                          ifelse(start_m==9 | start_m==10 | start_m==11, "Fall", 
-                                                 NA))))
-
-orig_data$end_s <- ifelse(end_m==12 | end_m==1 | end_m==2, "Winter", 
-                          ifelse(end_m==3 | end_m==4 | end_m==5, "Spring", 
-                                 ifelse(end_m==6 | end_m==7 | end_m==8, "Summer",
-                                        ifelse(end_m==9 | end_m==10 | end_m==11, "Fall", 
-                                               NA))))
-
-# 2a. Create dummy indicating if auction happened in car-demand peak season
-orig_data$peak_s = ifelse(orig_data$start_s=="Spring" | orig_data$start_s=="Fall",
-                          1, 0)
-
-# 2b. Create dummy indicating if start and end season are different
-orig_data$season_trans = ifelse(orig_data$start_s!=orig_data$end_s,
-                                1, 0)
-
-# 3. Var "length" is auction lasts in days, all set
-# 4. Var "endday" is weekday indicator (1,...,7), all set
-# 5. Var "endhour" is which the hour (24hr-clock) the auction ends, all set
-# 6. Var "endsunday" is a dummy pointing if endday is Sunday, all set
-
-# 7. Among "biddate1" "bidhour1" "bidminute1" "bidsecond1":
-## 7a. Calculate how long it took for the highest bid to appear
-orig_data$max_evot = orig_data$biddate1 - orig_data$startingdate
-
-# 7Note: All others from "biddate2" to "bidsecond22"...are no longer useful here 
-
-
 
 # Setting correct variable types
 # -------------------------------
