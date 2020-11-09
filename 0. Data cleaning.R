91a87aaab2cd7b977529298f10ffac52717b32da
Alston <alston005@gmail.com>
2020-11-09 12:37:18 -0600

    completed data cleaning

--- 0. Data cleaning.R
+++ 0. Data cleaning.R
@@ -103,83 +103,63 @@ orig_data$max_evot = orig_data$biddate1 - orig_data$startingdate
 
 # Setting correct variable types
 # -------------------------------
-## Create a full list of var names 
+## Create a full list of var names for debug
 all <- colnames(orig_data)
-all
-## A list of var names with good types
+## A list of var names with good types for debug
 list <- c()
 
 # -------------
-# Working on character variables
-list <- append(list,'sellerid')
-df2 <- orig_data['sellerid']
+## character
+orig_data$itemnum <- as.factor(orig_data$itemnum)
+list <- c('itemnum')
+all <- all[which(!all %in% list)]
 
-orig_data <- orig_data[,-which(colnames(orig_data) %in% list)] ##update data set unchecked
+## factor
+condition <- c("^ding*","^scratch*","^dent*","^broken*","^crack*","^problem*","^rust*")  #list all condition relevant vars
+fac_list <- c()
+for (i in condition){
+  vec <- colnames(orig_data)[grepl(i,colnames(orig_data))]
+  fac_list <- append(fac_list,vec)
+}
 
-# Date variables:
-# Time relevant int
-biddate <- colnames(orig_data)[grepl("^biddate*",colnames(orig_data))]
-bidhour <- colnames(orig_data)[grepl("^bidhour*",colnames(orig_data))]
-bidminute <- colnames(orig_data)[grepl("^bidminute*",colnames(orig_data))]
-bidsecond <- colnames(orig_data)[grepl("^bidsecond*",colnames(orig_data))]
-int.time <- c('year',biddate,bidhour,bidminute,bidsecond,'endhour','endingdate','startingdate','endday')
-df2 <- cbind(df2,suppressWarnings(lapply(X=orig_data[int.time], FUN=as.integer)))
-orig_data <- orig_data[,-which(colnames(orig_data) %in% int.time)] ##update data set unchecked
-list <- append(list,int.time)
+fac_list <- c(fac_list,'inspection','relistflag','featured','phone','addedinfo',
+              'endsunday','primetime','warranty','relist','sell','dealer',
+              'maker','interior','exterior','location','software','caradphotos')  #list all factors
+orig_data <- orig_data %>% mutate_at(.vars = fac_list, .funs = as.factor)
+list <- append(list,fac_list)
+all <- all[which(!all %in% list)]
 
-# Int:
-# Post feature Int: 
-int <- c('text','itemnum','miles','numbids','address','store','pwrseller',
-         'highbidderfdback','reserve','buyitnow','sellfdbackpct','photos',
-         'descriptionsize','options','doors','trans','webpage','title',
-         'condition','model','cyl','length','age','age2','html','sellerborn',
-         'week','auction','n','totallisted','totalsold','carmodel',
-         'ageXphoto','warrantyXphoto','warrantyXage','warrantyXageXphotos',
-         'group','auc_mins','maxbidtime','auc_duration_mins','max_evot')
+## Integers
+time <- c("^biddate*","^bidhour*","^bidminute*","^bidsecond*")
+int_list <- c()
+for (i in time){
+  vec <- colnames(orig_data)[grepl(i,colnames(orig_data))]
+  int_list <- append(int_list,vec)
+}
+int_list <- c(int_list,'year','endhour','endingdate','startingdate','endday','text',
+              'miles','numbids','address','store','pwrseller','highbidderfdback',
+              'reserve','buyitnow','sellfdbackpct','photos','descriptionsize','options','doors',
+              'trans','webpage','title','condition','model','cyl','length','age','age2','html',
+              'sellerborn','week','auction','n','totallisted','totalsold','maxbidtime',
+              'auc_duration_mins','max_evot')
+orig_data <- orig_data %>% mutate_at(.vars = int_list, .funs = as.integer)
+list <- append(list,int_list)
+all <- all[which(!all %in% list)]
 
-cbind(df2,suppressWarnings(lapply(X=orig_data[int], FUN=as.integer)))
-orig_data <- orig_data[,-which(colnames(orig_data) %in% int)] ##update data set unchecked
-list <- append(list,int)
 
 # Numeric
-num <- c('bookvalue','pctfdback','startbid','sellerage','negpct','compindex',c(paste0('biddy',1:22)))
-lognum <- c('logmiles','logtext','logsize','logstart','logfdback','logphotos',
-            'loghtml','logage','logbook',c(paste0('logbid',1:3)))
-num.full <- c(num,lognum)
-df2 <- cbind(df2,suppressWarnings(lapply(X=orig_data[num.full], FUN=as.numeric)))
-orig_data <- orig_data[,-which(colnames(orig_data) %in% num.full)] ##update data set unchecked
-list <- append(list,num.full)
+num_list <- c('bookvalue','pctfdback','startbid','sellerage','negpct','compindex',c(paste0('biddy',1:22)),
+              'logmiles','logtext','logsize','logstart','logfdback','logphotos',
+              'loghtml','logage','logbook',c(paste0('logbid',1:3)))
+orig_data <- orig_data %>% mutate_at(.vars = num_list, .funs = as.numeric)
+list <- append(list,num_list)
+all <- all[which(!all %in% list)]
 
-# factor
-ding <- colnames(orig_data)[grepl("^ding*",colnames(orig_data))] # assign all dummy vars relavant to 'ding'
-scratch <- colnames(orig_data)[grepl("^scratch*",colnames(orig_data))]
-dent <- colnames(orig_data)[grepl("^dent*",colnames(orig_data))]
-broken <- colnames(orig_data)[grepl("^broken*",colnames(orig_data))]
-crack <- colnames(orig_data)[grepl("^crack*",colnames(orig_data))]
-problem <- colnames(orig_data)[grepl("^problem*",colnames(orig_data))]
-rust <- colnames(orig_data)[grepl("^rust*",colnames(orig_data))]
+glimpse(orig_data[all])  ## Remaining vars are all in good type
 
-contmode <- colnames(orig_data)[grepl("^contcarmode*",colnames(orig_data))]
-contyr <- colnames(orig_data)[grepl("^contyear*",colnames(orig_data))]
-contwk <- colnames(orig_data)[grepl("^contweek*",colnames(orig_data))]
-con2mode <- colnames(orig_data)[grepl("^con2carmode*",colnames(orig_data))]
-con2yr <- colnames(orig_data)[grepl("^con2year*",colnames(orig_data))]
-con2n <- colnames(orig_data)[grepl("^con2n*",colnames(orig_data))]
-con2wk <- colnames(orig_data)[grepl("^con2week*",colnames(orig_data))]
-
-factor <- c('inspection','relistflag','featured','phone','addedinfo',
-            ding,scratch,dent,broken,crack,problem,rust,
-            'endsunday','primetime','warranty','relist','sell','dealer',
-            'maker','interior','exterior','location','software','caradphotos',
-            contmode,contyr,contwk,con2n)
-df2 <- cbind(df2,lapply(X=orig_data[factor], FUN=as.factor))
-orig_data <- orig_data[,-which(colnames(orig_data) %in% factor)] ##update data set unchecked
-list <- append(list,factor)
-
-orig_data <- cbind(df2,orig_data)
-rm(df2)  
-
-
+rm(all,i,list,num_list,time,vec, condition, fac_list, int_list) ##Remove useless values
+dim(orig_data)
+glimpse(orig_data)
 # ------------------------------------------------------------------
 # Sachin's part
 # I have checked the "orig_data" data file and there are no completely empty columns in it.
